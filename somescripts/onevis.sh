4b15c10e1e6c573d54e720afa633be1b3369a40e
Allison Kubo <alli.i.kubo@gmail.com>
2019-09-09 03:34:42 -0700

    added onevis.sh which make a top view at t=8

---
+++ somescripts/onevis.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+
+module load OpenDX
+
+echo sim name 
+#cd $1
+
+name=${PWD##*/} 
+echo $name
+
+cp /home/akubo/myprojects/channelized-pdcs/opendx/SEPT* ./
+cp /home/akubo/myprojects/channelized-pdcs/opendx/topo* ./
+
+
+dx -nodisplay -execonly -script SEPTkubo_maptoplane.net
+
+#mkdir ~/myprojects/graphics/visuals/$sim
+#cp *tif* ~/myprojects/graphics/visuals/$sim
+datum=$(date +"%D %R")
+
+echo -e "$datum ran SEPTkubo_mapptoplane.net" >>   status.txt
+echo -e "$datum ran SEPTkubo_mapptoplane.net" >> /home/akubo/myprojects/channelized-pdcs/status_all.txt
+
