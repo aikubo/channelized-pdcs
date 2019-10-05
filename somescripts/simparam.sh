c4ba67302c91c464d34261eeaed0cb12028ba342
Allison Kubo <alli.i.kubo@gmail.com>
2019-10-05 01:08:32 -0700

    updated simparam and newsim to reflect new parameter of amplitude

--- somescripts/simparam.sh
+++ somescripts/simparam.sh
@@ -4,12 +4,14 @@ simparam=$(grep -i "$1" /home/akubo/myprojects/channelized-pdcs/simulations.txt)
 
 label=$(echo $simparam | cut -d\, -f1)
 wave=$(echo $simparam | cut -d\, -f2)
-width=$(echo $simparam | cut -d\, -f3)
-depth=$(echo $simparam | cut -d\, -f4)
-height=$(echo $simparam | cut -d\, -f5)
+amp=$(echo $simparam | cut -d\, -f3)
+width=$(echo $simparam | cut -d\, -f4)
+depth=$(echo $simparam | cut -d\, -f5)
+height=$(echo $simparam | cut -d\, -f6)
 
 #echo $label 
 echo $wave
+echo $amp
 echo $width
 echo $depth 
 echo $height
