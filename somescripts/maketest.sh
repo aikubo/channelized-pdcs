ddfb6f2e8d7fc4606b9d54f7a9212646573ff230
Allison Kubo <alli.i.kubo@gmail.com>
2019-11-19 13:54:17 -0800

    fixed maketest.sh

--- somescripts/maketest.sh
+++ somescripts/maketest.sh
@@ -80,7 +80,7 @@ ifort -c -convert big_endian column.f90
 ifort -c -convert big_endian average.f90
 ifort -c -convert big_endian richardson.f90
 
-ifort var_3d.o postmod.o formatmod.o headermod.o average.o column.o richardson.o massinchannel.o entrainment.o findhead.o constants.o openbin.o openascii.o allocate_arrays.o handletopo.o post.f90  -convert big_endian -o test.exe
+ifort var_3d.o postmod.o formatmod.o headermod.o average.o column.o richardson.o massinchannel.o entrainment.o findhead.o constants.o openbin.o openascii.o allocate_arrays.o handletopo.o test.f90  -convert big_endian -o test.exe
 
 
 cp test.exe $here 
