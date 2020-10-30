409e43902b587d343c5db5efec9688a77d623a03
aikubo <alli.i.kubo@gmail.com>
2020-10-30 16:00:21 -0700

    added count spills

--- postprocessing/adv_post/openbin.f90
+++ postprocessing/adv_post/openbin.f90
@@ -1,43 +1,42 @@
 module openbinary 
-  contains  
+  contains
     subroutine openbin(numunit, filen, varname)
-        use parampost 
+        use parampost
         use constants
-        implicit none 
-        
+        implicit none
+
         integer:: II
-     
+
         integer, intent(IN) :: numunit
         character(LEN=*), intent(in) :: filen
         DOUBLE PRECISION, DIMENSION(:,:), INTENT(OUT) :: varname
-       
+
         OPEN(UNIT=numunit, file=filen, form='unformatted')
         REWIND(numunit)
                 print*, "opening bin"
                 DO II=1,timesteps
                         READ(numunit) varname(:,II)
                 END DO
-    
+
    end subroutine openbin
 
    subroutine opennotbin(numunit, filen,varname)
-        use parampost 
-        use constants 
+        use parampost
+        use constants
 
         integer, intent(IN) :: numunit
         character(LEN=*), intent(in) :: filen
         DOUBLE PRECISION, DIMENSION(:,:), INTENT(OUT) :: varname
 
        integer::II
-        
+
        open( unit=numunit, file=filen, form='formatted')
         print*, "opening", filen
-        
+
         do II=1,timesteps
                 Read(numunit,*) varname(:,II)
-        end do 
-  end subroutine        
+        end do
+  end subroutine
 
 
 end module openbinary
-
